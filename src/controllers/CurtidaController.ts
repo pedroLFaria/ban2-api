a2ec473d34bdb8b10c8242375842010646dd2b64
Pedro Luiz de Faria da Luz <pedro.luiz.fl@gmail.com>
2022-07-01 21:29:00 -0300

    Update CurtidaController.ts

--- src/controllers/CurtidaController.ts
+++ src/controllers/CurtidaController.ts
@@ -1,66 +1,25 @@
-import pool from "../db/dbconnector";
 import { Request, Response } from "express";
+import curtidaRepository from "../repository/curtidaRepository";
+import curtidaDto from "../models/CurtidaDto";
 
 class CurtidaController {
-  public async post(req: Request, res: Response) {
-    try {
-      const dbClient = await pool.connect();
-      const body = req.body;
-      const sql = `INSERT INTO public.curtida(
-        "usuarioId", "usuarioAlvoId", "isCurtida", "curtidaData")
-        VALUES (${body.usuarioId}, ${body.usuarioAlvoId}, ${body.isCurtida}, now()) 
-        ON CONFLICT ("usuarioId", "usuarioAlvoId") DO UPDATE
-        SET "isCurtida"=${body.isCurtida},
-        "curtidaData"= now();`;
-      const { rows } = await dbClient.query(sql);
-      const todos = rows;
 
-      dbClient.release();
-
-      res.send({ data: todos }).status(200);
-    } catch (error) {
-      res.status(500).send(error);
-    }
+  public async post(req: Request, res: Response) {    
+    const dto = req.body as curtidaDto;
+    curtidaRepository.insert(dto);
+    res.send().status(200);
   }
 
-  public async getMatches(req: Request, res: Response) {
-    try {
-      const dbClient = await pool.connect();
-      const userId = req.params.id;
-      const sql = `SELECT  u."usuarioNome",
-            u."usuarioId"
-        FROM public.curtida ca
-        join public.usuario u on u."usuarioId" = ca."usuarioId"
-        where ca."usuarioAlvoId" = ${userId} and ca."isCurtida"=true and ca."usuarioId" in (
-            SELECT c."usuarioAlvoId"
-                FROM public.curtida c
-                where c."usuarioId" = ${userId} and c."isCurtida"=true);`;
-      const { rows } = await dbClient.query(sql);
-      const todos = rows;
-
-      dbClient.release();
-
-      res.send({ data: todos }).status(200);
-    } catch (error) {
-      res.status(500).send(error);
-    }
+  public async getMatches(req: Request, res: Response) {    
+    const userId = req.params.id as number;
+    const curtidas = curtidaRepository.getMatches(userId);
+    res.send({ data: curtidas }).status(200);
   }
 
   public async delete(req: Request, res: Response) {
-    try {
-      const dbClient = await pool.connect();
-      const userId = req.params.id;
-      const sql = ` DELETE FROM public.curtida
-      WHERE "usuarioId" = ${userId};`;
-      const { rows } = await dbClient.query(sql);
-      const todos = rows;
-
-      dbClient.release();
-
-      res.send({ data: todos }).status(200);
-    } catch (error) {
-      res.status(500).send(error);
-    }
+      const userId = req.params.id as number;
+      curtidaRepository.delete(userId);
+      res.send().status(200);
   }
 }
 
