9e0c796f5cd25102d7d8ff46d5ee5a7f3d5946b9
Pedro Luiz de Faria da Luz <pedro.luiz.fl@gmail.com>
2022-07-11 01:25:55 -0300

    Create firebase-service-account.ts

---
+++ src/firebase/firebase-service-account.ts
@@ -0,0 +1,9 @@
+import { ServiceAccount } from "firebase-admin";
+
+const serviceAccount: ServiceAccount = {  
+  projectId: "--PROJECT_ID--",  
+  privateKey: "-----PRIVATE_KEY-----",
+  clientEmail: "--CLIENT_EMAIL---",
+}
+
+export default serviceAccount;
